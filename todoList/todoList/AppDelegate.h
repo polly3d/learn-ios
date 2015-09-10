5493ac90aa64043f3e866f40fa02d684c2cbe141
Polly3D <polly3d@126.com>
2015-09-10 11:17:49 +0800

    add todoList

---
+++ todoList/todoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  todoList
+//
+//  Created by wang on 15/9/10.
+//  Copyright (c) 2015年 wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
